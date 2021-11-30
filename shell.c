d7a83596edf2a70d95125f7ec6dfe2ecb5427a64
DavidASC20 <djchong24@gmail.com>
2021-11-30 16:58:41 -0500

    finished redirection

--- shell.c
+++ shell.c
@@ -29,6 +29,24 @@ void useCd(char *line){
 	}
 }	
 
+int command(char ** args) {
+    int f = fork();
+    int w, status;
+    if (f) {
+        w = waitpid(f, &status, 0);
+        return WEXITSTATUS(status);
+    } else {
+        int err = execvp(args[0], args);
+        if(err != 0) {
+		printf("error, Message is: %s\n", strerror(errno));
+		return err;        
+        }
+        exit(0);
+    }
+    return 0;
+}
+
+
 int main(int argc, char* argv[]){
 	while(1){
 		char currdir[100];
@@ -45,16 +63,45 @@ int main(int argc, char* argv[]){
 		}
 		
 		else if(strcmp(arguments[0],"exit") == 0){
-			break;
+			printf("Thanks for comin!\n");
+			return 0;
 		}
 		
-		else if (0) {
-		int f = fork();
-		int w, status;
-		if(f) {
-		 w = wait();
+		else{
+			char ** arg_copy = arguments;
+			int stdin_dup = dup(STDIN_FILENO);
+			int stdout_dup = dup(STDOUT_FILENO);
+			
+			int stdin_redir = -1;
+			int stdout_redir = -1;
+			while(*arg_copy){
+				if(strcmp(*arg_copy, "<") == 0) {
+					stdin_redir = open(arg_copy[1], O_RDONLY, 0777);
+					dup2(stdin_redir, STDIN_FILENO);
+					*arg_copy = 0;
+				}
+				else if(strcmp(*arg_copy, ">") == 0) {
+					stdout_redir = open(arg_copy[1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+					dup2(stdout_redir, STDOUT_FILENO);
+					*arg_copy = 0;
+				}
+				else if(strcmp(*arg_copy, ">>") == 0) {
+					stdout_redir = open(arg_copy[1], O_WRONLY | O_CREAT | O_APPEND, 0777);
+					dup2(stdout_redir, STDOUT_FILENO);
+					*arg_copy = 0;
+				}
+				arg_copy++;
+			}
+			command(arguments);
+			if(stdin_redir > -1) {
+				close(stdin_redir);
+			}
+			if(stdout_redir > -1) {
+				close(stdout_redir);
+			}
+			dup2(stdin_dup, STDIN_FILENO);
+			dup2(stdout_dup, STDOUT_FILENO);
 		}
-	}
 	}
 	return 0;
 }	
